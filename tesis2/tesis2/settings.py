1db8fd6caf1e2eee8968c1f4092d136460bee293
Lecknol9 <118777761+Lecknol@users.noreply.github.com>
2025-09-24 02:02:27 -0300

    Se cambio sqlite por posgresql

--- tesis2/tesis2/settings.py
+++ tesis2/tesis2/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'tesis2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'klorbis_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
